d34b39f67fe7bb01737e1d0ae91c4d167814c427
Kai Windle <kaiwindle@gmail.com>
2015-06-16 11:47:11 +0100

    Added a way to stop playback if the view controller is removed from the stack

--- AVKitDemo/Controller/KWMediaPlayerViewController.m
+++ AVKitDemo/Controller/KWMediaPlayerViewController.m
@@ -28,6 +28,32 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)viewDidDisappear:(BOOL)animated {
+    
+    [super viewDidDisappear:animated];
+    
+    if ([self isPlaying]) {
+        // stop the video playback
+        [self stopPlayback];
+    }
+}
+
+-(void)stopPlayback {
+    
+    [self.player setRate:0];
+    self.player = nil;
+}
+
+-(BOOL)isPlaying {
+    
+    if (self.player.currentItem && self.player.rate > 0) {
+        
+        return YES;
+    }
+    
+    return NO;
+}
+
 -(void)setupVideoPlayback {
     
     NSURL *url          = [[NSBundle mainBundle] URLForResource:self.videofile withExtension:@"mp4"];
@@ -35,7 +61,7 @@
     AVURLAsset *asset   = [AVURLAsset URLAssetWithURL:url options:nil];
     AVPlayerItem *item  = [AVPlayerItem playerItemWithAsset:asset];
     
-    self.player = [AVPlayer playerWithPlayerItem:item];
+    self.player         = [AVPlayer playerWithPlayerItem:item];
     [self.player seekToTime:kCMTimeZero];
     [self.player play];
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AVKitDemo
 Gives a simple example of how to use the AVKit framework to play video for iOS 8/9 Written in Xcode 7 beta 1
+This example is not the best in the world but will give a very basic idea of what to do to get AVPlayerViewController to work
